00d1e4680b88cf0145272654cb7112312354f993
devi9113624552 <dpraidola@gmail.com>
2025-04-02 09:54:14 +0530

    feat(cart): Add cart model

---
+++ api/model/cart.js
@@ -0,0 +1,37 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const cartItemSchema = new Schema ({
+  productId: {
+    type: Schema.Types.ObjectId,
+    ref: 'product',
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    default: 1,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const cartSchema = new Schema (
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'auth',
+      required: true,
+    },
+    items: [cartItemSchema],
+    totalPrice: {
+      type: String,
+      required: true,
+    },
+  },
+  {timestamps: true}
+);
+
+module.exports = mongoose.model ('cart', cartSchema);
