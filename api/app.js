1f743b65494a3e1e63bc8a3f5eaae949d5677d5f
devi9113624552 <dpraidola@gmail.com>
2025-04-15 20:52:05 +0530

    feat(auth): Add redis for storing user sessions.

--- api/app.js
+++ api/app.js
@@ -6,7 +6,23 @@ const mongoose = require ('mongoose');
 const cors = require ('cors');
 const session = require ('express-session');
 const passport = require ('./util/passport');
-const User = require ('./model/auth');
+// redis connection
+const Redis = require ('redis');
+const {RedisStore} = require ('connect-redis');
+
+const RedisClient = Redis.createClient ({
+  username: 'default',
+  password: 'WJW0qykBGNjhnjlDoSLRsk6PdsVWdS8l',
+  socket: {
+    host: 'redis-13548.c264.ap-south-1-1.ec2.redns.redis-cloud.com',
+    port: 13548,
+  },
+});
+RedisClient.connect ().catch (err =>
+  logger.error (`Error while connecting redis:${err}`)
+);
+
+let redisStore = new RedisStore ({client: RedisClient, prefix: 'store:'});
 
 const app = express ();
 const PORT = process.env.PORT || 8080;
@@ -23,17 +39,24 @@ app.use (bodyParser.json ());
 //session middleware to store passport data
 app.use (
   session ({
+    store: redisStore,
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 60 * 60 * 1000,
+    },
   })
 );
 app.use (passport.initialize ());
 app.use (passport.session ());
 
 app.use (async (req, res, next) => {
-  const user = await User.findById ('67ed4783773af80c554f0128');
-  req.user = user;
+  if (req.session.user) {
+    req.user = req.session.user;
+  }
   next ();
 });
 
