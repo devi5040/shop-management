a6148c303b3a28da81a2e3a87ead916545c0a86a
devi9113624552 <dpraidola@gmail.com>
2025-04-19 22:16:54 +0530

    feat(auth): Add auth middleware for cart routes

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -1,25 +1,32 @@
 const express = require ('express');
 const router = express.Router ();
 const cartController = require ('../controller/cart');
+const {checkAuth} = require ('../util/auth');
 
 // route definition for getting cart items
-router.get ('/', cartController.getCart);
+router.get ('/', checkAuth, cartController.getCart);
 
 // route definition for adding item to cart
-router.post ('/add-cart', cartController.addToCart);
+router.post ('/add-cart', checkAuth, cartController.addToCart);
 
 //route definition for removing an item from the cart
-router.post ('/remove-item/:productId', cartController.deleteCartItem);
+router.post (
+  '/remove-item/:productId',
+  checkAuth,
+  cartController.deleteCartItem
+);
 
 // route definition for incrementing cart quantity
 router.post (
   '/increment-quantity/:productId',
+  checkAuth,
   cartController.incrementQuantity
 );
 
 // route definition for decrementing cart quantity
 router.post (
   '/decrement-quantity/:productId',
+  checkAuth,
   cartController.decrementQuantity
 );
 
