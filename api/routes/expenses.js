215444a99061e9ad3ec851398fc593342fe7b7d5
devi9113624552 <dpraidola@gmail.com>
2025-04-20 22:35:59 +0530

    Added auth middleware for expenses routes.

--- api/routes/expenses.js
+++ api/routes/expenses.js
@@ -2,24 +2,39 @@ const express = require ('express');
 const router = express.Router ();
 const {uploadIfBillExists} = require ('../util/fileHelper');
 const expenseController = require ('../controller/expenses');
+const {checkAuth} = require ('../util/auth');
 
 // route definition for fetching all expenses
-router.get ('/', expenseController.getAllExpenses);
+router.get ('/', checkAuth, expenseController.getAllExpenses);
 
 // route definition for fetching detail for an expense
-router.get ('/expense-details/:expenseId', expenseController.getExpense);
+router.get (
+  '/expense-details/:expenseId',
+  checkAuth,
+  expenseController.getExpense
+);
 
 // route definition for adding the expense
-router.post ('/add-expense', uploadIfBillExists, expenseController.addExpense);
+router.post (
+  '/add-expense',
+  checkAuth,
+  uploadIfBillExists,
+  expenseController.addExpense
+);
 
 // route definition for updating the expense
 router.put (
   '/update-expense/:expenseId',
+  checkAuth,
   uploadIfBillExists,
   expenseController.updateExpense
 );
 
 // route definition for deleting the expense
-router.delete ('/delete-expense/:expenseId', expenseController.deleteExpense);
+router.delete (
+  '/delete-expense/:expenseId',
+  checkAuth,
+  expenseController.deleteExpense
+);
 
 module.exports = router;
