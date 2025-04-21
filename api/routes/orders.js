46002ad46b6a16074a91c3a6c438cdd32990938e
devi9113624552 <dpraidola@gmail.com>
2025-04-21 22:37:27 +0530

    Added auth middleware for orders routes.

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,17 +1,22 @@
 const express = require ('express');
 const router = express.Router ();
 const orderController = require ('../controller/orders');
+const {checkAuth} = require ('../util/auth');
 
 // route definition for getting the order details
-router.get ('/', orderController.getOrderDetails);
+router.get ('/', checkAuth, orderController.getOrderDetails);
 
 // route definition for creating an order
-router.post ('/create-order', orderController.createOrder);
+router.post ('/create-order', checkAuth, orderController.createOrder);
 
 // route definition for getting the order status
-router.get ('/order-status/:orderId', orderController.getOrderStatus);
+router.get (
+  '/order-status/:orderId',
+  checkAuth,
+  orderController.getOrderStatus
+);
 
 // route definition for cancelling the order
-router.post ('/cancel-order/:orderId', orderController.cancelOrder);
+router.post ('/cancel-order/:orderId', checkAuth, orderController.cancelOrder);
 
 module.exports = router;
